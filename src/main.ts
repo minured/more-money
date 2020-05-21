2b9562e2700480ff9399527f9868960bf3f83210
minured <1296900813@qq.com>
2020-05-21 14:58:42 +0800

    引入全局的 Nav，各页面分别使用

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
 
 //如果是引入目录，那么默认是引入该目录下的index，
 //这里就是 ./router/index.ts
-import router from './router'
-import store from './store'
+import router from './router';
+import store from './store';
+import Nav from '@/components/Nav.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+//一个组件在多个组件使用，把它注册为全局组件
+Vue.component('Nav', Nav);
 new Vue({
   //初始化router，router:router
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    Label.vue
+    Labels.vue
+    <Nav/>
   </div>
 </template>
 
 <script lang="ts">
   export default {
-    name: 'Label'
+    name: 'Label',
   };
 </script>
 
