c6e0b41a06608f0f7ff5a86be52b995b4787ab18
minured <1296900813@qq.com>
2020-05-24 22:46:45 +0800

    模块化：把Money分成四个子组件

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="tags">
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+    <ul class="current">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Tags'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .tags {
+    padding: 16px;
+    font-size: 14px;
+    /*border: 1px solid black;*/
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column-reverse;
+
+    > .current {
+      /*border: 1px solid black;*/
+      display: flex;
+
+      > li {
+        /*border: 1px solid black;*/
+        /*display: block;*/
+        margin-right: 16px;
+        height: 24px;
+        line-height: 24px;
+        padding: 0 16px;
+        border-radius: 12px;
+        text-align: center;
+        background-color: #d9d9d9;
+      }
+    }
+
+    > .new {
+      padding-top: 16px;
+
+      button {
+        background-color: transparent;
+        border: none;
+        /*不给color默认跟字体颜色一样*/
+        border-bottom: 1px solid;
+        color: #999;
+        /*边框超出字体*/
+        padding: 0 4px;
+
+
+      }
+    }
+  }
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="types">
+    <li class="selected">支出</li>
+    <li>收入</li>
+  </ul>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'types'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .types {
+    background-color: #c4c4c4;
+    display: flex;
+    font-size: 24px;
+
+    > li {
+      width: 50%;
+      /*border: 1px solid black;*/
+      height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+
+      &.selected::after {
+        content: "";
+        display: block;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: #333;
+
+      }
+    }
+  }
+</style>
