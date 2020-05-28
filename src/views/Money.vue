8bdfb860490c7eb195a4525f97db0353c2eeb2c7
minured <1296900813@qq.com>
2020-05-28 23:10:33 +0800

    update

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -30,20 +30,20 @@
     date?: Date;
   }
 
-  const records: Record[] = JSON.parse(window.localStorage.getItem('records') || '[]');
+  const localRecords: Record[] = JSON.parse(window.localStorage.getItem('records') || '[]');
   //数据库版本 ，用来升级数据库
   //版本判断升级
   const version = window.localStorage.getItem('version') || 0;
   if (version === '0.0.1') {
-    records.forEach(record => {
+    localRecords.forEach(record => {
       record.date = new Date(0);
     });
-    window.localStorage.setItem('records', JSON.stringify(records));
-
+    window.localStorage.setItem('records', JSON.stringify(localRecords));
   }
   window.localStorage.setItem('version', '0.0.2');
 
 
+  //使用的时候，要注意，下面是Vue里面，要注意this，上面是外面
   @Component({
     components: {Tags, Notes, Types, NumberPad},
   })
@@ -58,15 +58,15 @@
       date: new Date()
     };
 
-
+    records: Record[] = localRecords
     saveRecord() {
       this.record.date = new Date();
       //这里push的是一个引用，所以每次push应该创建一个新的对象
       //用JSON实现深拷贝
       const deepClone: Record = JSON.parse(JSON.stringify(this.record));
-      records.push(deepClone);
+      this.records.push(deepClone);
+      console.log(this.records)
     }
-
     //用watch  避免漏保存
     @Watch('records')
     onRecordsChange() {
